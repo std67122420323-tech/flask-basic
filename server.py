39d34ae0050cfc9358eb9ff8b3dfe6a410a41852
std67122420323-tech <std.67122420323@ubru.ac.th>
2025-12-09 12:06:22 +0700

    Update server.py

--- server.py
+++ server.py
@@ -21,27 +21,27 @@ def index():
     Return: return_description
     """
     
-  @app.route('/name') 
-  def name(): 
+@app.route('/name') 
+def name(): 
       return "<h1>surachai</h1>"
 
 @app.route('/user/<username>')
 def user(username): 
     return f"<h1>my name is {username}</h1>"
 
-@app.route('calculate/addition/<int:a>/<int:b>')
+@app.route('/calculate/addition/<int:a>/<int:b>')
 def addition(a, b): 
     return f"<{a}+{b}: {a + b}</h1>"
 
-@app.route('calculate/subtraction/<int:a>/<int:b>')
+@app.route('/calculate/subtraction/<int:a>/<int:b>')
 def subtraction(a, b): 
     return f"<{a}-{b}: {a - b}</h1>"
 
-@app.route('calculate/multiplication/<int:a>/<int:b>')
+@app.route('/calculate/multiplication/<int:a>/<int:b>')
 def multiplication(a, b): 
     return f"<{a}*{b}: {a * b}</h1>"
 
-@app.route('calculate/division/<int:a>/<int:b>')
+@app.route('/calculate/division/<int:a>/<int:b>')
 def division(a, b): 
     return f"<{a}/{b}: {a / b}</h1>"
 
